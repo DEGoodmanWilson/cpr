f8bcfd2470a0e0d3908346863267e72fadf058c0
Don Goodman-Wilson <don@slack-corp.com>
2018-02-05 08:29:42 +0100

    bitrot

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class CPRConan(ConanFile):
     version = "1.2.0"
     url = "https://github.com/whoshuu/cpr.git"
     license = "MIT"
-    requires = "libcurl/7.49.1@lasote/stable"
+    requires = "libcurl/7.56.1@bincrafters/stable"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
                "insecure_curl": [True, False],
